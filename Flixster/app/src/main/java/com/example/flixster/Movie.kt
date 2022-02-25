7363b085a79f04973317f9dd13352ab74451d50a
weigan610 <tylerzhang610@gmail.com>
2022-02-25 16:24:15 -0500

    test commit

--- Flixster/app/src/main/java/com/example/flixster/Movie.kt
+++ Flixster/app/src/main/java/com/example/flixster/Movie.kt
@@ -1,4 +1,31 @@
 package com.example.flixster
 
-class Movie {
+import org.json.JSONArray
+
+data class Movie(
+    val title: String,
+    val movieId: Int,
+    val overview: String,
+    private val posterPath: String,
+) {
+    val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+    companion object {
+        fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
+            val movies = mutableListOf<Movie>()
+
+            for (i in 0 until movieJsonArray.length()) {
+                val movieJson = movieJsonArray.getJSONObject(i)
+                movies.add(
+                    Movie (
+                        movieJson.getString("original_title"),
+                        movieJson.getInt("id"),
+                        movieJson.getString("overview"),
+                        movieJson.getString("poster_path")
+                    )
+                )
+            }
+
+            return movies
+        }
+    }
 }
--- Flixster/app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ Flixster/app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,2 +1,36 @@
-package com.example.flixster 
+package com.example.flixster
 
+import android.content.Context
+import android.view.*
+import android.widget.*
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class MovieAdapter(private val context: Context, private val movies: List<Movie>)
+    : RecyclerView.Adapter<MovieAdapter.ViewHolder> () {
+
+    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder (itemView) {
+        private val ivPoster = itemView.findViewById<ImageView> (R.id.ivPoster)
+        private val tvTitle = itemView.findViewById<TextView> (R.id.tvTitle)
+        private val tvOverview = itemView.findViewById<TextView> (R.id.tvOverview)
+
+        fun bind (movie: Movie) {
+            tvTitle.text = movie.title
+            tvOverview.text = movie.overview
+            Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val movie = movies[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+}
